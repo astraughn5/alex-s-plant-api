1d6ecf1064e2ee086335970d90a00b5a89f3b3c8
astraughn5 <astraughn5@gmail.com>
2022-06-02 15:49:53 -0500

    updating code

--- client-side-code/js/main.js
+++ client-side-code/js/main.js
@@ -1,9 +1,9 @@
 document.querySelector('button').addEventListener('click', apiRequest)
 
 async function apiRequest(){
-    const rapperName = document.querySelector('input').value
+    const plantName = document.querySelector('input').value
     try{
-        const response = await fetch(`https://alex-plant-api.herokuapp.com/api/${rapperName}`)
+        const response = await fetch(`https://alex-plant-api.herokuapp.com/api/${plantName}`)
         const data = await response.json()
 
         console.log(data)
